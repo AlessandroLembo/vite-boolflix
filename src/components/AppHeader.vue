beae46d9e67c35af818016c0839a8e8bf4df5b39
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-26 20:50:45 +0100

    implement method with call for tv series

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,19 +6,24 @@ export default {
     data() {
         return {
             movies: [],
+            series: [],
             baseUri: 'https://api.themoviedb.org/3/',
             apiKey: 'dd9b1073e48ec92b6d25fb7c351682de',
-            index: 0
         }
     },
 
     methods: {
-        fetchChoosenMovies(item) {
+        fetchChoosenContent(item) {
             axios.get(`${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${item}&language=IT-it`)
                 .then((res) => {
                     this.movies = res.data.results
-                    this.getFlagString(i);
+                });
+
+            axios.get(`${this.baseUri}/search/tv?api_key=${this.apiKey}&query=${item}&language=IT-it`)
+                .then((res) => {
+                    this.series = res.data.results
                 })
+
         },
 
 
@@ -29,9 +34,9 @@ export default {
 </script>
 
 <template>
-    <search-content @start-research="fetchChoosenMovies"></search-content>
+    <search-content @start-research="fetchChoosenContent"></search-content>
     <ul>
-        <li v-for="(movie, i) in movies" :key="movie.id">
+        <li v-for="movie in movies" :key="movie.id">
             <h1> {{ movie.original_title }} </h1>
             <p v-if="movie.title !== movie.original_title"> {{ movie.title }} </p>
             <figure>
@@ -43,6 +48,15 @@ export default {
 
         </li>
     </ul>
+    <h1>---------------------------------------------------------------------</h1>
+    <ul>
+        <li v-for="serie in series" :key="serie.id">
+            <h1> {{ serie.name }} </h1>
+
+        </li>
+    </ul>
+
+
 </template>
 
 <style>
