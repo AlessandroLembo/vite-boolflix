f0befbc3d25dee6851e5fae5f2decf9baecd07a1
AlessandroLembo <alessandrolembo2@gmail.com>
2023-01-26 23:45:51 +0100

    moved axios calls in App from AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,61 +1,19 @@
 <script>
-import axios from 'axios';
 import SearchContent from './SearchContent.vue';
 export default {
+    name: 'AppHeader',
     components: { SearchContent },
-    data() {
-        return {
-            movies: [],
-            series: [],
-            baseUri: 'https://api.themoviedb.org/3/',
-            apiKey: 'dd9b1073e48ec92b6d25fb7c351682de',
-        }
-    },
-
     methods: {
-        fetchChoosenContent(item) {
-            axios.get(`${this.baseUri}/search/movie?api_key=${this.apiKey}&query=${item}&language=IT-it`)
-                .then((res) => {
-                    this.movies = res.data.results
-                });
-
-            axios.get(`${this.baseUri}/search/tv?api_key=${this.apiKey}&query=${item}&language=IT-it`)
-                .then((res) => {
-                    this.series = res.data.results
-                })
-
-        },
-
-
-
-    },
+        fetchChoosenContent(word) {
+            this.$emit('filter-content', word);
+        }
+    }
 
 }
 </script>
 
 <template>
     <search-content @start-research="fetchChoosenContent"></search-content>
-    <ul>
-        <li v-for="movie in movies" :key="movie.id">
-            <h1> {{ movie.original_title }} </h1>
-            <p v-if="movie.title !== movie.original_title"> {{ movie.title }} </p>
-            <figure>
-                <img v-if="movie.original_language === 'en'" src="../assets/flags/en.png" alt="en">
-                <img v-else-if="movie.original_language === 'it'" src="../assets/flags/it.png" alt="it">
-                <p v-else> {{ movie.original_language }} </p>
-            </figure>
-            <p> {{ movie.vote_average }} </p>
-
-        </li>
-    </ul>
-    <h1>---------------------------------------------------------------------</h1>
-    <ul>
-        <li v-for="serie in series" :key="serie.id">
-            <h1> {{ serie.name }} </h1>
-
-        </li>
-    </ul>
-
 
 </template>
 
--- src/components/SearchContent.vue
+++ src/components/SearchContent.vue
@@ -1,5 +1,6 @@
 <script>
 export default {
+    name: 'SearchContent',
     data() {
         return {
             searchValue: ''
